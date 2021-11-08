9513536e4e898db0f5d66fd7a943df53eb4b5ccc
Efrain <efrainc.dev@gmail.com>
2021-11-08 11:40:25 -0500

    Api Loopback

---
+++ apiloopback/src/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from './ping.controller';
+export * from './usuariocontroller.controller';
+export * from './aeropuertocontroller.controller';
+export * from './rutacontroller.controller';
+export * from './vueloscontroller.controller';
+export * from './ruta-aeropuerto.controller';
+export * from './vuelo-ruta.controller';
